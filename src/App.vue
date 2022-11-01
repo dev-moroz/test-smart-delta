79648f60300ece0333c37fcb3ca788041b8116d4
dev-moroz <dev_moroz@mail.ru>
2022-11-01 23:36:14 +0300

    upload project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CardVue />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardVue from './components/CardVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardVue
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  align-items: center;
 }
 </style>
---
+++ src/components/CardVue.vue
@@ -0,0 +1,63 @@
+<template>
+   <div class="card">
+      <div class="title">
+         <img alt="subtract" src="../assets/Subtract.svg">
+         <p>{{ title }}</p>
+      </div>
+      <button>{{ button }}</button>
+   </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default{
+   setup(){
+      const title = ref('Title Big mob')
+      const button = ref('Label mob')
+      return {
+         title,
+         button
+      }
+   }
+}
+
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap');
+
+.card{
+   background: linear-gradient(90deg, #E81F2F 0%, #FF8983 100%);
+   border-radius: 20px;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   padding: 0 8px 0 16px;
+   max-height: 52px;
+   max-width: 375px;
+   min-width: 260px;
+   width: 100%;
+}
+.title{
+   display: flex;
+   gap: 10px;
+}
+p{
+   color: #fff;
+   font-weight: 700;
+   font-family: 'Montserrat', sans-serif;
+}
+button{
+   color: #E81F2F;
+   background-color: #fff;
+   border-radius: 12px;
+   padding: 8px 16px;
+   border: none;
+   font-weight: 500;
+   font-size: 14px;
+   line-height: 20px;
+}
+button:hover{
+   cursor: pointer;
+}
+</style>
